33970d83e02366b5eeda1f3eda3bca8ea1758bf6
AdamNafea-STUDENT <g00462155@atu.ie>
2026-09-16 14:45:50 +0100

    Implement ProductController with GET and POST endpoints

--- src/main/java/com/example/catalogservice/controller/ProductController.java
+++ src/main/java/com/example/catalogservice/controller/ProductController.java
@@ -1,4 +1,27 @@
 package com.example.catalogservice.controller;
 
+import com.example.catalogservice.model.Product;
+import com.example.catalogservice.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+    private final ProductService productService;
+
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+    @GetMapping
+    public List<Product> getProducts() {
+        return productService.getAll();
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.create(product);
+    }
 }
